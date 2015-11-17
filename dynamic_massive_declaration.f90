84c7e2468d3309f202583c3dd24e8a712f48dce3
Vladimir Onoprienko <vonopr@ya.ru>
2015-11-17 12:58:04 +0300

    A folder without OTPS

---
+++ dynamic_massive_declaration.f90
@@ -0,0 +1,25 @@
+program White_Sea_Coordinates
+
+        implicit none 
+     integer, allocatable :: a(:,:) 
+     integer, parameter :: M = 5 
+     integer i,j 
+     allocate(a(M,M)) 
+          forall(i=1:M,j=1:M) 
+          a(i,j) = i+j
+          endforall 
+   
+     do i=1,M 
+     do j=1,M
+     write(*,"(i4)",ADVANCE='NO') a(i,j) 
+     enddo  
+     write(*,*)
+     enddo
+
+deallocate(a) 
+
+
+
+ 
+    
+end program White_Sea_Coordinates
